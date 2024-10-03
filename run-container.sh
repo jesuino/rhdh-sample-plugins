ea10263f160807a85b9eef33ed0e2c42223046f3
William Siqueira <william.fatecsjc@gmail.com>
2024-10-03 16:21:55 -0300

    Updating to RHDH version 1.3

---
+++ run-container.sh
@@ -0,0 +1,1 @@
+podman run -v ./deploy:/opt/app-root/src/dynamic-plugins-root:Z -v ./app-config.local.yaml:/opt/app-root/src/app-config.local.yaml:Z -p 7007:7007 --entrypoint='["node", "packages/backend", "--config", "app-config.yaml", "--config", "app-config.example.yaml", "--config", "app-config.local.yaml"]' quay.io/rhdh/rhdh-hub-rhel9:1.3
